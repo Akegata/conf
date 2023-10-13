02438be6f65671b60e9c04620653bbe5c3996f5a
akegata@gmail.com <akegata@gmail.com>
2023-10-13 19:30:13 +0200

    .

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+vim.api.nvim_set_keymap('n', '<C-o>', ':Neotree toggle<CR>', { noremap = true, silent = true })
