43ce45e0c1343932de3bcf3db4c1916ae23df585
akegata@gmail.com <akegata@gmail.com>
2023-11-02 23:04:58 +0100

    .

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
 	{ "folke/flash.nvim", enabled = false },
-	{ "folke/noice.nvim", enabled = false },
+	-- { "folke/noice.nvim", enabled = false },
+	-- { "rcarriga/nvim-notify", enabled = false },
 }
