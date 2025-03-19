b2fe6904457a349e873019665a58aed5adfd615d
marhov <martin.hovmoller@kindredgroup.com>
2025-03-19 15:37:31 +0100

    Updated nvim keymaps.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.api.nvim_set_keymap("n", "<C-l>", ":Neotree toggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":Neotree toggle<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-o>", ":Neotree focus<CR>", { noremap = true, silent = true })
+
+-- Move left with Ctrl+hjkl
+-- vim.api.nvim_del_keymap('i', '<C-k>')  -- Remove mapping for C-k
+vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true })
