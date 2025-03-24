942fe28a8ef43e78fbcd9b6330738330cfd804c7
akegata@gmail.com <akegata@gmail.com>
2025-03-24 17:07:48 +0100

    Added conf for leap.nvim.

--- nvim/.config/nvim/lua/plugins/leap-nvim.lua
+++ nvim/.config/nvim/lua/plugins/leap-nvim.lua
@@ -1,11 +1,16 @@
 return {
-  -- add symbols-outline
-  -- lazy.nvim
-  {
-    "ggandor/leap.nvim",
-     keys = {
-       { "s", mode = { "n", "x", "o" }, "<Plug>(leap-forward)", desc = "Leap  orward" },
-       { "S", mode = { "n", "x", "o" }, "<Plug>(leap-backward)", desc = "Leap backward" },
-    },
-  },
+	-- add symbols-outline
+	-- lazy.nvim
+	{
+		"ggandor/leap.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, "<Plug>(leap-forward)", desc = "Leap  orward" },
+			{ "S", mode = { "n", "x", "o" }, "<Plug>(leap-backward)", desc = "Leap backward" },
+		},
+		config = function()
+			require("leap").opts.safe_labels = {}
+			-- You might also want to add the basic setup if it's not already done elsewhere
+			require("leap").add_default_mappings()
+		end,
+	},
 }
